f76bff6cabe88539157258ed2d1fb3b968503b54
lequangdo <dole@mpire.vn>
2019-06-05 18:56:51 +0700

    begin

--- rpi-host-server.go
+++ rpi-host-server.go
@@ -2,10 +2,15 @@ package main
 
 import (
     "fmt"
-   // "html/template"
-)
+//    "html/template"
+    "net/http"
 
+)
+func hello (w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Welcome to my website!")
+}
 func main() {    
 	fmt.Println("starting")
-
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":9000", nil)
 }
